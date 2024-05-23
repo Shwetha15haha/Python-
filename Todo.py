d45d84183c1d62521f51081cee5009044d90421d
Shwetha M <88648469+Shwetha15haha@users.noreply.github.com>
2024-05-23 22:32:32 +0530

    Add files via upload
    Added file

---
+++ Todo.py
@@ -0,0 +1,12 @@
+prompt = "Enter to do:"
+# user input
+Task1 = input(prompt)
+Task2 = input(prompt)
+Task3 = input(prompt)
+
+Todolist = [Task1, Task2, Task3]
+print(Todolist)
+
+print(type(Todolist))
+
+
