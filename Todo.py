318207b56132e2dbd04987ca9e815b90e82832c6
moghe <mogheshwetha15@gmail.com>
2024-05-23 22:27:40 +0530

    Comments added

--- Todo.py
+++ Todo.py
@@ -1,23 +1,30 @@
+# the msg variable for user to enter the to do
 prompt = "Enter to do:"
 
+# define an empty list outside the while loop
 todos = []
+
+# while True ask user to enter the to do.
+# use title method to format each to do
+# append each to do to empty list
+# print a list of all the todos
 while True:
     todo = input(prompt)
-    todo = todo.capitalize()
+    todo = todo.title()
     todos.append(todo)
     print(todos)
 
 # user input
-""" Task1 = input(prompt)
-Task2 = input(prompt)
-Task3 = input(prompt)
-
-Todolist = [Task1, Task2, Task3]
-print(Todolist)
-
-print(type(Todolist))
-
-print("Length of the task1:", len(Task1))
-print("Length of the task2:", len(Task2))
-print("Length of the task3:", len(Task3))
-"""
+# Task1 = input(prompt)
+# Task2 = input(prompt)
+# Task3 = input(prompt)
+#
+# Todolist = [Task1, Task2, Task3]
+# print(Todolist)
+#
+# print(type(Todolist))
+#
+# print("Length of the task1:", len(Task1))
+# print("Length of the task2:", len(Task2))
+# print("Length of the task3:", len(Task3))
+#
