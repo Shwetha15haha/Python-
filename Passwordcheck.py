c546a07c7efd8eadb07826ef8f41814ce536b52e
moghe <mogheshwetha15@gmail.com>
2024-05-23 22:23:40 +0530

    Comments added

--- Passwordcheck.py
+++ Passwordcheck.py
@@ -1,8 +1,11 @@
-
+# ask user to enter password
 password = input("Enter the password:")
 
+# as long as the password entered is not equal to the actual value
+# keep asking user to enter password
 while password != 'password123':
     password = input("Enter the password:")
 
+# if user enters right password print below msg
 print("Password is correct")
 
