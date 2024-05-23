5b2fe90c17b3032e33dd3c8831682a5836930a56
moghe <mogheshwetha15@gmail.com>
2024-05-23 22:03:37 +0530

    Input given using while loop

--- Todo.py
+++ Todo.py
@@ -1,6 +1,14 @@
 prompt = "Enter to do:"
+
+todos = []
+while True:
+    todo = input(prompt)
+    todo = todo.capitalize()
+    todos.append(todo)
+    print(todos)
+
 # user input
-Task1 = input(prompt)
+""" Task1 = input(prompt)
 Task2 = input(prompt)
 Task3 = input(prompt)
 
@@ -12,4 +20,4 @@ print(type(Todolist))
 print("Length of the task1:", len(Task1))
 print("Length of the task2:", len(Task2))
 print("Length of the task3:", len(Task3))
-
+"""
